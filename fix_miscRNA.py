b064c288a0cc2554d8c8cdd6476c72c950d98ba7
Jon Badalamenti <jonb@umn.edu>
2016-02-22 19:03:02 -0600

    added help

--- fix_miscRNA.py
+++ fix_miscRNA.py
@@ -3,14 +3,22 @@
 from Bio import SeqIO
 import sys
 import re
+import argparse
+from argparse import RawTextHelpFormatter
 
-# silence Biopython warnings of improper indentation of Genbank files and CDS that are not multiples of 3
+# silence Biopython warnings
 import warnings
 from Bio import BiopythonParserWarning
 from Bio import BiopythonWarning
 warnings.simplefilter('ignore', BiopythonParserWarning)
 warnings.simplefilter('ignore', BiopythonWarning)
 
+# script help and usage
+parser=argparse.ArgumentParser(
+    description='collects statistics of codon presence/absence and total count per locus tag',
+    epilog='Author: Jon Badalamenti, Bond Lab, University of Minnesota (http://www.thebondlab.org)\nhttp://github.com/jbadomics/genbank_submit\nFebruary 2016\n \n', formatter_class=RawTextHelpFormatter)
+parser.add_argument('[GENBANK FILE]', help='Genbank file to correct ydaO-yuaA miscRNA features')
+args=parser.parse_args()
 # define input file handle
 genbankFile = open(sys.argv[1], 'r')
 
