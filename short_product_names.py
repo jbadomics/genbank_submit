591d8e36121604040693d6b2ec2e242776d94993
Jon Badalamenti <jonb@umn.edu>
2016-02-22 18:28:50 -0600

    added help

--- short_product_names.py
+++ short_product_names.py
@@ -3,13 +3,22 @@
 from Bio import SeqIO
 import sys
 import re
+import argparse
+from argparse import RawTextHelpFormatter
 
-# silence Biopython warnings of improper indentation of Genbank files and CDS that are not multiples of 3
+# silence Biopython warnings
 import warnings
 from Bio import BiopythonParserWarning
 from Bio import BiopythonWarning
 warnings.simplefilter('ignore', BiopythonParserWarning)
 warnings.simplefilter('ignore', BiopythonWarning)
+
+# script help and usage
+parser=argparse.ArgumentParser(
+    description='prints locus tags for any CDS whose product is <= 6 characters\nThese should be BLASTed for more informative annotations where possible\n\nRequires BioPython v. 1.65 or later (http://biopython.org/wiki/Download)', 
+    epilog='Author: Jon Badalamenti, Bond Lab, University of Minnesota (http://www.thebondlab.org)\nhttp://github.com/jbadomics/genbank_submit\nFebruary 2016\n \n', formatter_class=RawTextHelpFormatter)
+parser.add_argument('[GENBANK FILE]', help='Genbank file to screen for long product names')
+args=parser.parse_args()
 
 # define input file handle
 genbankFile = open(sys.argv[1], 'r')
