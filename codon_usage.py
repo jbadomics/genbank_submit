3b962bbe1b11550b946ec12c2e17e3f1ab4d23f8
Jon Badalamenti <jonb@umn.edu>
2016-02-22 19:00:03 -0600

    added check for abnormal codon lengths

--- codon_usage.py
+++ codon_usage.py
@@ -18,13 +18,17 @@ warnings.simplefilter('ignore', BiopythonWarning)
 
 # script help and usage
 parser=argparse.ArgumentParser(
-    description='extracts amino acid sequences of hypothetical proteins FASTA format from a (multi)-Genbank file.',
-    epilog='Author: Jon Badalamenti, Bond Lab, University of Minnesota (http://www.thebondlab.org)\nhttp://github.com/jbadomics/tnseq\nFebruary 2016\n \n', formatter_class=RawTextHelpFormatter)
+    description='collects statistics of codon presence/absence and total count per locus tag',
+    epilog='Author: Jon Badalamenti, Bond Lab, University of Minnesota (http://www.thebondlab.org)\nhttp://github.com/jbadomics/genbank_submit\nFebruary 2016\n \n', formatter_class=RawTextHelpFormatter)
 parser.add_argument('[GENBANK FILE]', help='Genbank file from which to extract genome sequence')
 parser.add_argument('[CODON]', help='three-letter DNA codon of interest (e.g. ATG, not AUG)')
 args=parser.parse_args()
 
-codon = sys.argv[2]
+codon = sys.argv[2].upper().replace("U", "T")
+
+if len(codon) % 3 != 0:
+	print 'codon provided is not a multiple of 3'
+	sys.exit()
 
 outputFileName = sys.argv[1].replace(".gbk", "." + codon + ".txt")
 proteins_list = []
@@ -47,8 +51,13 @@ with open(outputFileName, 'wb') as outputFile:
 						endCoord = int(feature.location.start.position) #this is not a typo
 						geneSequence = str(genome[endCoord:startCoord].reverse_complement())
 						codonList = [geneSequence[i:i+3] for i in range(0, len(geneSequence))]
-					if 'ATA' in codonList:
+					if codon in codonList:
 						proteins_list.append(locusTag)
 						outputString = '%s\t%i' % (locusTag, codonList.count(codon))
 						outputFile.write(outputString + "\n")
-print '%i proteins contain at least one %s codon' % (len(proteins_list), codon)
+if len(proteins_list) > 0:
+	print '%i proteins contain at least one %s codon' % (len(proteins_list), codon)
+	print 'results written to %s' % (outputFileName)
+else:
+	print '%s not found' % (codon)
+
