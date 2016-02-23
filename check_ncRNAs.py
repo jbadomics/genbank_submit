ce99c9e671eadf07c10cc3a3bd1450ba2a93a263
Jon Badalamenti <jonb@umn.edu>
2016-02-22 18:23:59 -0600

    removed extraneous comments

--- check_ncRNAs.py
+++ check_ncRNAs.py
@@ -3,18 +3,25 @@
 from Bio import SeqIO
 import sys
 import re
+import argparse
+from argparse import RawTextHelpFormatter
 
-# silence Biopython warnings of improper indentation of Genbank files and CDS that are not multiples of 3
+# silence Biopython warnings
 import warnings
 from Bio import BiopythonParserWarning
 from Bio import BiopythonWarning
 warnings.simplefilter('ignore', BiopythonParserWarning)
 warnings.simplefilter('ignore', BiopythonWarning)
 
+# script help and usage
+parser=argparse.ArgumentParser(
+    description='prints ncRNA features lacking required ncRNA_class descriptor for manual inspection\n\nRequires BioPython v. 1.65 or later (http://biopython.org/wiki/Download)', 
+    epilog='Author: Jon Badalamenti, Bond Lab, University of Minnesota (http://www.thebondlab.org)\nhttp://github.com/jbadomics/genbank_submit\nFebruary 2016\n \n', formatter_class=RawTextHelpFormatter)
+parser.add_argument('[GENBANK FILE]', help='Genbank file to search for problematic ncRNAs')
+args=parser.parse_args()
+
 # define input file handle
 genbankFile = open(sys.argv[1], 'r')
-
-outputFile = open('output.gbk', 'w')
 
 for sequenceRecord in SeqIO.parse(genbankFile, 'genbank'):
 
@@ -23,15 +30,7 @@ for sequenceRecord in SeqIO.parse(genbankFile, 'genbank'):
 		if feature.type == "ncRNA":
 		
 			if feature.qualifiers["ncRNA_class"] == None:
-				# noteString = ''.join(feature.qualifiers["product"]) + " riboswitch"
-				# feature.qualifiers["note"] = noteString
-				# feature.qualifiers.pop("product", None)
 				locusTag = ''.join(feature.qualifiers["locus_tag"])
 				print locusTag
-				# feature.qualifiers.pop("locus_tag", None)
-
-	SeqIO.write(sequenceRecord, outputFile, "genbank")
-
-outputFile.close()
 
 genbankFile.close()
