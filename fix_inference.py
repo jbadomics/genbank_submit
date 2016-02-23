76b7cbc70d02cb1d6effe96fd5b69a18aff259e9
Jon Badalamenti <jonb@umn.edu>
2016-02-22 18:45:55 -0600

    added help

--- fix_inference.py
+++ fix_inference.py
@@ -3,13 +3,22 @@
 from Bio import SeqIO
 import sys
 import re
+import argparse
+from argparse import RawTextHelpFormatter
 
-# silence Biopython warnings of improper indentation of Genbank files and CDS that are not multiples of 3
+# silence Biopython warnings
 import warnings
 from Bio import BiopythonParserWarning
 from Bio import BiopythonWarning
 warnings.simplefilter('ignore', BiopythonParserWarning)
 warnings.simplefilter('ignore', BiopythonWarning)
+
+# script help and usage
+parser=argparse.ArgumentParser(
+    description='corrects the format of Prokka-derived \inference strings in a GenBank file\nInspect your GenBank file and modify line 38 to match the accession numbers shown in \inference qualifiers\n\nRequires BioPython v. 1.65 or later (http://biopython.org/wiki/Download)',
+    epilog='Author: Jon Badalamenti, Bond Lab, University of Minnesota (http://www.thebondlab.org)\nhttp://github.com/jbadomics/genbank_submit\nFebruary 2016\n \n', formatter_class=RawTextHelpFormatter)
+parser.add_argument('[GENBANK FILE]', help='Genbank file from which to extract hypothetical protein sequences')
+args=parser.parse_args()
 
 # define input file handle
 genbankFile = open(sys.argv[1], 'r')
@@ -24,11 +33,12 @@ for sequenceRecord in SeqIO.parse(genbankFile, 'genbank'):
 		
 			if len(feature.qualifiers["inference"]) >= 2:
 				inferenceString = ''.join(feature.qualifiers["inference"][1])
+				
+				# Modify this search string as needed depending on your situation
 				proteinAccession = re.search('\|YP_.*\|', inferenceString)
 				
 				if proteinAccession:
 					newInferenceString = "similar to AA sequence:RefSeq:" + proteinAccession.group(0).replace("|", "")
-					print newInferenceString
 					feature.qualifiers["inference"][1] = newInferenceString
 
 	SeqIO.write(sequenceRecord, outputFile, "genbank")
--- get_hypotheticals.py
+++ get_hypotheticals.py
@@ -19,7 +19,7 @@ warnings.simplefilter('ignore', BiopythonWarning)
 # script help and usage
 parser=argparse.ArgumentParser(
     description='extracts amino acid sequences of hypothetical proteins FASTA format from a (multi)-Genbank file\n\nRequires BioPython v. 1.65 or later (http://biopython.org/wiki/Download)',
-    epilog='Author: Jon Badalamenti, Bond Lab, University of Minnesota (http://www.thebondlab.org)\nhttp://github.com/jbadomics/tnseq\nFebruary 2016\n \n', formatter_class=RawTextHelpFormatter)
+    epilog='Author: Jon Badalamenti, Bond Lab, University of Minnesota (http://www.thebondlab.org)\nhttp://github.com/jbadomics/genbank_submit\nFebruary 2016\n \n', formatter_class=RawTextHelpFormatter)
 parser.add_argument('[GENBANK FILE]', help='Genbank file from which to extract hypothetical protein sequences')
 args=parser.parse_args()
 
